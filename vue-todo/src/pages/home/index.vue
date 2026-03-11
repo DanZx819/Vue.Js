11dc8fc872f2fdc877108c41354d1eab8f291aac
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2026-03-11 19:29:38 -0300

    Setando layouts

--- vue-todo/src/pages/home/index.vue
+++ vue-todo/src/pages/home/index.vue
@@ -1,7 +1,12 @@
-<template>
-  <h1>Daniel</h1>
-</template>
 
 <script setup>
-  
+  import DefaultLayout from '@/layouts/DefaultLayout.vue';
 </script>
+
+<template>
+  <DefaultLayout>
+    <h1>Daniel</h1>
+  </DefaultLayout>
+  
+</template>
+
