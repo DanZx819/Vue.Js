00dacd4a9b020a14e6e6d67b01dddd9fc95ae92b
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2026-03-13 16:23:17 -0300

    Organizando projeto com pinia

--- vue-todo/src/components/DialogDeleteComponent.vue
+++ vue-todo/src/components/DialogDeleteComponent.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="text-center pa-4">
-    <v-dialog v-model="props.dialog" max-width="400" persistent>
+    <v-dialog v-model="taskStore.showDialogDelete" max-width="400" persistent>
       <v-card title="Delete task ?" text="Do you want delete this task ?">
-        <v-card-text>{{ task.title }}</v-card-text>
+        <v-card-text>{{ props.task?.title }}</v-card-text>
         <template v-slot:actions>
           <v-spacer></v-spacer>
 
-          <v-btn @click="$emit('toggle')"> No </v-btn>
+          <v-btn @click="taskStore.toggleDelete()"> No </v-btn>
 
-          <v-btn @click="$emit('deleteTask')"> Yes </v-btn>
+          <v-btn @click="taskStore.deleteTask()"> Yes </v-btn>
         </template>
       </v-card>
     </v-dialog>
@@ -16,10 +16,12 @@
 </template>
 
 <script setup>
-import { ref, defineProps } from "vue";
+import { ref } from "vue";
+import {useTaskStore} from "@/stores/task.js";
+
+const taskStore = useTaskStore();
 
 const props = defineProps({
-  dialog: Boolean,
   task: Object,
 });
 
